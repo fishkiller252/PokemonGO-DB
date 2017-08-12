d2443d51c0aee2f0e9afec2d99b185b9c019ecef
Unknown <fishkiller.1017@gmail.com>
2017-08-13 02:23:38 +0900

    pokemonDB

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<link rel="shortcut icon" href="images/icon.ico" />
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/menu.css" />
+	<title>ポケモンGO種族値データベース</title>
+</head>
+
+<body>
+
+  <div id="menu_frame">
+    <iframe src="menu.html" name="top" scrolling="auto" width="100%" height="46" frameborder="0" border="0">フレーム対応ブラウザで閲覧ください。</iframe>
+  </div>
+
+	<?php
+	// データベースに接続する
+
+	$pdo = new PDO("mysql:host=localhost;dbname=pdb;charset=utf8", "root", "");
+	// print_r($_POST);
+
+  ?>
+	<h1>ポケモンGO種族値データベース</h1>
+
+	<?php
+	// データベースからデータを取得する
+//$sql = "SELECT * FROM pokemondb ORDER BY  DEPT_NO, USER_ID DESC;";
+  $sql = "SELECT * FROM pokemondb ORDER BY  id;";
+	$stmt = $pdo->prepare($sql);
+	$stmt -> execute();
+	?>
+	<table border="1">
+		<tr>
+			<th>ポケモン名</th>
+  <!--<th>画像</th>-->
+      <th>攻撃力</th>
+			<th>守備力</th>
+			<th>HP</th>
+			<th>合計</th>
+			<th></th>
+		</tr>
+		<?php
+		// 取得したデータを表示する
+		while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) { ?>
+			<tr>
+				<td><?= $row["name"] ?></td>
+				<!--    header('Content-Type: image/png');
+			 	 <td>readfile(<?//php= $row["image"] ?>);</td> -->
+				<td><?= $row["attack"] ?></td>
+				<td><?= $row["defense"] ?></td>
+				<td><?= $row["hp"] ?></td>
+				<td><?= $row["total"] ?></td>
+				<td>
+					</form>
+				</td>
+			</tr>
+		<?php } ?>
+	</table>
+</body>
+
+</html>
